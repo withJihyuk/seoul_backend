dcad8f604e539455e4809dc1fe253c18e8c509fd
Jihyuk Lee <lead@sver.dev>
2024-05-10 08:03:17 +0900

    :fire: :: 거리 계산 기능 제거

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
 from db import r as db
-import json, calculate
-from pydantic import BaseModel
+import json
 
 app = FastAPI()
-
-class location(BaseModel): 
-    X_COORD: float
-    Y_COORD: float
-
 
 @app.get("/")
 def root():
@@ -23,11 +17,3 @@ def getCulturalEventInfo():
 def stadiumScheduleInfo():
     data = db.get('culturalSpaceInfo')
     return json.loads(data)
-
-@app.post("/stadiumScheduleLoactionCal")
-async def stadiumScheduleLoactionCal(item: location):
-    data = json.loads(db.get('culturalSpaceInfo').decode('utf-8'))
-    OWN_X_COORD = item.X_COORD
-    OWN_Y_COORD = item.Y_COORD
-    nearest_events = calculate.find_nearest_events(OWN_X_COORD, OWN_Y_COORD, data, 3)
-    return nearest_events
